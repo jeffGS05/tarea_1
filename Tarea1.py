941244797a137d0ac6d9a22100df36df38a19e80
jeffGS05 <49353053+jeffGS05@users.noreply.github.com>
2019-04-08 15:19:03 -0600

    Add files via upload

---
+++ Tarea1.py
@@ -0,0 +1,9 @@
+from math import pi
+
+New_pi = pi
+#print("********* %.9s\n" % New_pi, "******** %.8s\n" % New_pi, "******* %.7s\n" % New_pi, "****** %.6s\n" % New_pi, "*** %.5s\n" % New_pi, "** %.4s\n"  % New_pi)
+print("********* %.4s %.9s" % New_pi)
+
+
+
+
